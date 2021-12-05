25df54daf86d44b72f293acc6a868b4225690d27
marcelinoNet <marcelinoneto34@gmail.com>
2021-12-05 11:14:50 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -18,3 +18,11 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-10-20T03:00:00Z' , TIMESTAMP WITH TIME ZONE '2021-10-20T03:00:00Z' , 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z' , TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z' , 1);
+
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'https://isi-mtl.com/wp-content/uploads/2020/01/c-project_management-1.jpg', 1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tire suas dúvidas',1,'https://isi-mtl.com/wp-content/uploads/2020/01/c-project_management-1.jpg', 2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives Exclusivas para a turma ',1,'https://isi-mtl.com/wp-content/uploads/2020/01/c-project_management-1.jpg', 0,1);
+
+
+
