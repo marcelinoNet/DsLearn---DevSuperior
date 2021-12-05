85883d42e7daf73749d28a970cdcbe97f7190822
marcelinoNet <marcelinoneto34@gmail.com>
2021-12-05 10:23:47 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,3 +13,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'https://isi-mtl.com/wp-content/uploads/2020/01/c-project_management-1.jpg', 'https://static.thenounproject.com/png/3387200-200.png');
